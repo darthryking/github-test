893f556195bfe174a7bd44564f12c2c8c5745f39
Amanda Lee <alee58@schools.nyc.gov>
2025-05-29 10:45:13 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! This is a new GitHub project.");
+        System.out.println(sum(3,4));
     }
     
     public static int sum(int x, int y){
